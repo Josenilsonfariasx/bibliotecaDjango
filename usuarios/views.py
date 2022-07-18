d3db36a1442720622a78f29c672f533b1df1c403
Josenilsonfariasx <fariaslwork@gmail.com>
2022-07-18 10:26:25 -0300

    Atualizando arquivos

--- usuarios/views.py
+++ usuarios/views.py
@@ -2,6 +2,8 @@ import email
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import redirect
+
+import usuarios
 from .models import Usuario
 from hashlib import sha256
 
@@ -13,6 +15,18 @@ def login(request):
 def cadastro(request):
     status = request.GET.get('status')
     return render(request, 'cadastro.html', {'status': status})
+
+def valida_login(request):
+    email_login = request.POST.get('Email-login')
+    senha_login = request.POST.get('Senha-login')
+
+    senha_login = sha256 (senha_login.encode()).hexdigest()
+
+    usuario = Usuario.objects.filter(email = email_login).filter(senha = senha_login)
+
+    if len(usuario) == 0:
+        return redirect('/auth/login/?status=0')
+    
 
 
 def valida_cadastro(request):
@@ -26,8 +40,8 @@ def valida_cadastro(request):
 
 
     if len(nome.strip()) == 0 or len(email.strip()) == 0:
-        return redirect('/auth/cadastro/?status=1')
-
+        return redirect('/auth/login/?status=1')
+        
 
     if len(senha) < 8:
         return redirect('/auth/cadastro/?status=2')
