a09060dbbaa90b65dc58cde0fe67535bec698f16
Josenilsonfariasx <fariaslwork@gmail.com>
2022-07-29 09:37:44 -0300

    Views.livro atualizando

--- livro/views.py
+++ livro/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from usuarios.models import Usuario
 # Create your views here.
 
 def home (request):
     if request.session.get('usuario'):
-        return HttpResponse ('Primeiro comando com um fremework')
+        usuario = Usuario.objects.get(id = request.session['usuario']).nome
+        return HttpResponse (f'Bem vindo {usuario}')   
     else:
         return redirect ('/auth/login/?status=2')
